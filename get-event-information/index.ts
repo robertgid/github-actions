a78f4d15be17a65c7aad354b3ec2d772a878d78a
antonSvetin <anton@global.id>
2022-01-14 17:43:26 +0100

    feat(actions): Adding deployment config into action - INFRA-983

--- get-event-information/index.ts
+++ get-event-information/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import * as github from '@actions/github'
 import * as core from '@actions/core'
 
@@ -8,6 +9,40 @@ const tagFormat = new RegExp(/^v([0-9]{1,}\.){2}[0-9]{1,}(-alpha\.[0-9]{1,})?/)
 const releaseFormat = new RegExp(/^v([0-9]{1,}\.){2}[0-9]{1,}$/)
 // eslint-disable-next-line unicorn/better-regex
 const prereleaseFormat = new RegExp(/^v([0-9]{1,}\.){2}[0-9]{1,}(-alpha\.[0-9]{1,})$/)
+
+interface EnvironmentContext {
+  environment: string
+  context: string
+  namespace: string
+  manifest: string
+}
+
+const deploymentValues: Record<string, EnvironmentContext> = {
+  eksDev: {
+    environment: 'eks-dev',
+    context: 'legacy-dev',
+    namespace: 'dev',
+    manifest: 'dev_manifest.yaml',
+  },
+  eksStaging: {
+    environment: 'eks-staging',
+    context: 'legacy-dev',
+    namespace: 'dev',
+    manifest: 'dev_manifest.yaml',
+  },
+  legacyDev: {
+    environment: 'legacy-dev',
+    context: 'legacy-dev',
+    namespace: 'dev',
+    manifest: 'dev_manifest.yaml',
+  },
+  legacyStg: {
+    environment: 'legacy-staging',
+    context: 'legacy-dev',
+    namespace: 'dev',
+    manifest: 'dev_manifest.yaml',
+  },
+}
 
 function run(): void {
   try {
@@ -28,16 +63,18 @@ function run(): void {
     core.notice(`Ref: ${ref}, short ref: ${shortRef}`)
     const tagName = ref.startsWith('refs/tags') ? shortRef : github.context.sha
     let releaseType: string = ''
-    let envName: string = ''
+    deploymentValues.eksDev.namespace = group
+    deploymentValues.eksStaging.namespace = group
+    let config: EnvironmentContext[] = []
     if (ref.startsWith('refs/tags') === true && tagFormat.test(tagName) === true) {
       if (releaseFormat.test(tagName) === true) {
         releaseType = 'release'
       } else if (prereleaseFormat.test(tagName) === true) {
         releaseType = 'prerelease'
       }
-      envName = 'stg'
+      config = getDeploymentConfig('staging')
     } else if (devBranches.test(ref)) {
-      envName = 'dev'
+      config = getDeploymentConfig('dev')
     }
 
     core.notice(`Group: ${group}`)
@@ -46,7 +83,7 @@ function run(): void {
     core.notice(`Tag name: ${tagName}`)
     core.notice(`Namespace: ${group}`)
     core.notice(`Release type: ${releaseType} - ${ref}`)
-    core.notice(`Environment name: ${envName}`)
+    core.notice(`Deploy config: ${JSON.stringify(config)}`)
 
     core.setOutput('group', group)
     core.setOutput('name', imageName)
@@ -54,9 +91,20 @@ function run(): void {
     core.setOutput('tag-name', tagName)
     core.setOutput('namespace', group)
     core.setOutput('release-type', releaseType)
-    core.setOutput('env-name', envName)
+    if (config.length > 0) {
+      core.setOutput('deploy-config', JSON.stringify(config))
+    }
   } catch (error) {
     core.setFailed((<Error>error).message)
+  }
+}
+
+function getDeploymentConfig(environment: string): EnvironmentContext[] {
+  switch (environment) {
+    case 'staging':
+      return [deploymentValues.legacyStg, deploymentValues.eksStaging]
+    default:
+      return [deploymentValues.legacyDev, deploymentValues.eksDev]
   }
 }
 
